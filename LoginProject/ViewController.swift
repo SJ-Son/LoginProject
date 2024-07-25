88fad234ff24e75e40f8b43442f63a85dca81c8b
sungjun son <o_nj_u@naver.com>
2024-07-25 14:38:21 +0900

    code based UI / AutoLayout

--- LoginProject/ViewController.swift
+++ LoginProject/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let emailTextFieldView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeUI()
+        
     }
-
-
+    
+    func makeUI() {
+        emailTextFieldView.backgroundColor = UIColor.darkGray
+        view.addSubview(emailTextFieldView)
+        // 자동으로 켜주는 autolayout 기능 끄기
+        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
+        // autolayout
+        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true  // 왼쪽
+        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true // 오른쪽
+        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true   // 위쪽
+        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true // 높이
+    }
+    
 }
 
